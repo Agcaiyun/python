0f0f2d7bb2030ea68cf6fae32c0780881d6b53f3
agcaiyun <agcaiyun@163.com>
2019-12-19 13:28:10 +0800

    change %d to %.1f

--- quadratic.py
+++ quadratic.py
@@ -6,11 +6,13 @@ def quadratic(a, b, c):
     sqrtNum = b  * b - 4 * a * c
     result1 = (( -b ) + math.sqrt(sqrtNum)) / ( 2 * a)
     result2 = (( -b ) - math.sqrt(sqrtNum)) / ( 2 * a)
+    print(sqrtNum)
+    print(result1)
+    print(result2)
+
     if sqrtNum > 0:
-        print('两个不等根：result1: %d, result2: %d' % (result1, result2))
+        print('两个不等根：result1: %.1f, result2: %.1f' % (result1, result2))
     elif sqrtNum == 0:
-        print('两个相等根：result1=result= %d' % result1 )
+        print('两个相等根：result1=result= %.1f' % result1 )
     else:
         print('无根')
-
-quadratic(1, 2, 0)
